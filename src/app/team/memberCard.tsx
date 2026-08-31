c79c38d6f67ddd5bd1932fa7ec1764bacb9f0ae8
Xavier Koch <xavier.koch07@gmail.com>
2026-08-31 04:20:03 -0400

    Trying out new design checking to see how it will look on regular laptop instead of desktop monitor. Also fixing some small issues with the team page and member card.

--- src/app/team/memberCard.tsx
+++ src/app/team/memberCard.tsx
@@ -12,7 +12,7 @@ export default function TeamMemberCard(props: TeamMemberProps) {
       <h3>{props.name}</h3>
       <span>{props.role}</span>
       <a href={props.link} target="_blank" rel="noopener noreferrer">
-        <img src="Images/LinkedinLogo.png" alt="LinkedIn Logo" />
+        <img src="images/LinkedinLogo.png" alt="LinkedIn Logo" />
       </a>
     </div>
   );
--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -37,7 +37,170 @@ export default function TeamPage() {
           link="https://www.linkedin.com/in/samir-chowdhury23/"
         />
       </div>
-      {/* Advisors */}
+      <div className = "department-grid">
+      <div className="dept-row">
+        {/* Technology Team */}
+        <div className="Tech-Team">
+          <h2>Tech Team</h2>
+          <TeamMemberCard
+            image="/images/Umad.png"
+            name="Umad"
+            role="Director of Technology"
+            link="https://www.linkedin.com/in/umad-akram/"
+          />
+          <div className="dept-officers-coordinators">
+          <TeamMemberCard
+            image="/images/Haseeb.png"
+            name="Haseeb"
+            role="Technology Officer"
+            link="https://www.linkedin.com/in/haseeb-khann/"
+          />
+          <TeamMemberCard
+            image="/images/Tabish.png"
+            name="Tabish"
+            role="Technology Officer"
+            link="https://www.linkedin.com/in/tabish-ghouri/"
+          />
+          <TeamMemberCard
+            image="/images/Xavier.png"
+            name="Xavier"
+            role="Technology Officer"
+            link="https://www.linkedin.com/in/xavier-koch-527bb2387/"
+          />
+          </div>
+        </div>
+        {/* Marketing Team */}
+        <div className="Marketing-Team">
+          <h2>Marketing Team</h2>
+          <TeamMemberCard
+            image="/images/Amna.png"
+            name="Amna"
+            role="Director of Marketing"
+            link="https://www.linkedin.com/in/amna--yousuf/"
+          />
+          <div className="dept-officers-coordinators">
+          <TeamMemberCard
+            image="/images/Amitav.png"
+            name="Amitav"
+            role="UI/UX Designer"
+            link="https://www.linkedin.com/in/amitav-sonawane/"
+          />
+          <TeamMemberCard
+            image="/images/Hemanti.png"
+            name="Hemanti"
+            role="Marketing Coordinator"
+            link="https://www.linkedin.com/in/hemanti-badam-9b6986396/"
+          />
+          <TeamMemberCard
+            image="/images/Qudsia.jpg"
+            name="Qudsia"
+            role="Marketing Coordinator"
+            link="" /* Waiting for linkdln */
+          />
+          <TeamMemberCard
+            image="/images/Atifa Baz photo.jpg"
+            name="Atifa"
+            role="Marketing Coordinator"
+            link="" /* Waiting for linkdln */
+          />
+          </div>
+        </div>
+        {/* Logistics Team */}
+        <div className="Logistics-Team">
+          <h2>Logistics Team</h2>
+          <TeamMemberCard
+            image="/images/Ihsan.png"
+            name="Ihsan"
+            role="Director of Logistics"
+            link="https://www.linkedin.com/in/mohammad-ihsan-wadid-47596b36b/"
+          />
+          <div className="dept-officers-coordinators">
+          <TeamMemberCard
+            image="/images/Aakash.png"
+            name="Aakash"
+            role="Tech Logistics Officer"
+            link="https://www.linkedin.com/in/aakashharen/"
+          />
+          <TeamMemberCard
+            image="/images/Akeell.png"
+            name="Akeell"
+            role="Logistics Officer"
+            link="https://www.linkedin.com/in/akeell-parameswaran/"
+          />
+          </div>
+          </div>
+        </div>
+        <div className="dept-row">
+        {/* Events Team */}
+        <div className="Events-Team">
+          <h2>Events Team</h2>
+          <TeamMemberCard
+            image="/images/Aravind.png"
+            name="Aravind"
+            role="Director of Events"
+            link="https://www.linkedin.com/in/aravindnira/"
+          />
+          <div className="dept-officers-coordinators">
+          <TeamMemberCard
+            image="/images/Thomas.jpg"
+            name="Thomas"
+            role="Events Coordinator"
+            link="https://www.linkedin.com/in/thomas-mitchinson/"
+          />
+          <TeamMemberCard
+            image="/images/Haris.png"
+            name="Haris"
+            role="Events Coordinator"
+            link="https://www.linkedin.com/in/haris-khan-915300246/"
+          />
+          <TeamMemberCard
+            image="/images/Bach.png"
+            name="Bach"
+            role="Events Officer"
+            link="https://www.linkedin.com/in/bach-nguyen-vu/"
+          />
+          <TeamMemberCard
+            image="/images/placeholder.jpg"
+            name="Zoha"
+            role="Events Officer"
+            link="" /* Waiting for linkdln */
+          />
+          </div>
+        </div>
+        {/* Sponsors Team */}
+        <div className="Sponsors-Team">
+          <h2>Sponsors Team</h2>
+          <TeamMemberCard
+            image="/images/Khalid.png"
+            name="Khalid"
+            role="Director of Sponsors"
+            link="https://linkedin.com/in/Khalid"
+          />
+          <div className="dept-officers-coordinators">
+          <TeamMemberCard
+            image="/images/Monty.png"
+            name="Monty"
+            role="Partnerships Officer"
+            link="https://www.linkedin.com/in/monty-abdi-94a5a8371/"
+          />
+          <TeamMemberCard
+            image="/images/Bushrat.png"
+            name="Bushrat"
+            role="Partnerships Officer"
+            link="https://www.linkedin.com/in/bushratzahan/"
+          />
+          <TeamMemberCard
+            image="/images/Rameen.png"
+            name="Rameen"
+            role="Partnerships Officer"
+            link="" /* Waiting for LinkedIn link */
+          />
+          </div>
+        </div>
+        {/* Advisors */}
+      
+        {/* Skeleton for Meet the Team Page */}
+      </div>
       <h2>Advisors</h2>
       <div className="Advisors">
         <TeamMemberCard
@@ -71,155 +234,7 @@ export default function TeamPage() {
           link="https://www.linkedin.com/in/edrees-amiri/"
         />
       </div>
-      <div className="department-grid">
-        {/* Technology Team */}
-        <div className="Tech-Team">
-          <h2>Tech Team</h2>
-          <TeamMemberCard
-            image="/images/Umad.png"
-            name="Umad"
-            role="Director of Technology"
-            link="https://www.linkedin.com/in/umad-akram/"
-          />
-          <TeamMemberCard
-            image="/images/Haseeb.png"
-            name="Haseeb"
-            role="Technology Officer"
-            link="https://www.linkedin.com/in/haseeb-khann/"
-          />
-          <TeamMemberCard
-            image="/images/Tabish.png"
-            name="Tabish"
-            role="Technology Officer"
-            link="https://www.linkedin.com/in/tabish-ghouri/"
-          />
-          <TeamMemberCard
-            image="/images/Xavier.png"
-            name="Xavier"
-            role="Technology Officer"
-            link="https://www.linkedin.com/in/xavier-koch-527bb2387/"
-          />
-        </div>
-        {/* Marketing Team */}
-        <div className="Marketing-Team">
-          <h2>Marketing Team</h2>
-          <TeamMemberCard
-            image="/images/Amna.png"
-            name="Amna"
-            role="Director of Marketing"
-            link="https://www.linkedin.com/in/amna--yousuf/"
-          />
-          <TeamMemberCard
-            image="/images/Amitav.png"
-            name="Amitav"
-            role="UI/UX Designer"
-            link="https://www.linkedin.com/in/amitav-sonawane/"
-          />
-          <TeamMemberCard
-            image="/images/Hemanti.png"
-            name="Hemanti"
-            role="Marketing Coordinator"
-            link="https://www.linkedin.com/in/hemanti-badam-9b6986396/"
-          />
-          <TeamMemberCard
-            image="/images/Qudsia.jpg"
-            name="Qudsia"
-            role="Marketing Coordinator"
-            link="" /* Waiting for linkdln */
-          />
-          <TeamMemberCard
-            image="/images/Atifa Baz photo.jpg"
-            name="Atifa"
-            role="Marketing Coordinator"
-            link="" /* Waiting for linkdln */
-          />
-        </div>
-        {/* Logistics Team */}
-        <div className="Logistics-Team">
-          <h2>Logistics Team</h2>
-          <TeamMemberCard
-            image="/images/Ihsan.png"
-            name="Ihsan"
-            role="Director of Logistics"
-            link="https://www.linkedin.com/in/mohammad-ihsan-wadid-47596b36b/"
-          />
-          <TeamMemberCard
-            image="/images/Aakash.png"
-            name="Aakash"
-            role="Tech Logistics Officer"
-            link="https://www.linkedin.com/in/aakashharen/"
-          />
-          <TeamMemberCard
-            image="/images/Akeell.png"
-            name="Akeell"
-            role="Logistics Officer"
-            link="https://www.linkedin.com/in/akeell-parameswaran/"
-          />
-        </div>
-        {/* Events Team */}
-        <div className="Events-Team">
-          <h2>Events Team</h2>
-          <TeamMemberCard
-            image="/images/Aravind.png"
-            name="Aravind"
-            role="Director of Events"
-            link="https://www.linkedin.com/in/aravindnira/"
-          />
-          <TeamMemberCard
-            image="/images/Thomas.jpg"
-            name="Thomas"
-            role="Events Coordinator"
-            link="https://www.linkedin.com/in/thomas-mitchinson/"
-          />
-          <TeamMemberCard
-            image="/images/Haris.png"
-            name="Haris"
-            role="Events Coordinator"
-            link="https://www.linkedin.com/in/haris-khan-915300246/"
-          />
-          <TeamMemberCard
-            image="/images/Bach.png"
-            name="Bach"
-            role="Events Officer"
-            link="https://www.linkedin.com/in/bach-nguyen-vu/"
-          />
-          <TeamMemberCard
-            image="/images/placeholder.jpg"
-            name="Zoha"
-            role="Events Officer"
-            link="" /* Waiting for linkdln */
-          />
-        </div>
-        {/* Sponsors Team */}
-        <div className="Sponsors-Team">
-          <h2>Sponsors Team</h2>
-          <TeamMemberCard
-            image="/images/Khalid.png"
-            name="Khalid"
-            role="Director of Sponsors"
-            link="https://linkedin.com/in/Khalid"
-          />
-          <TeamMemberCard
-            image="/images/Monty.png"
-            name="Monty"
-            role="Partnerships Officer"
-            link="https://www.linkedin.com/in/monty-abdi-94a5a8371/"
-          />
-          <TeamMemberCard
-            image="/images/Bushrat.png"
-            name="Bushrat"
-            role="Partnerships Officer"
-            link="https://www.linkedin.com/in/bushratzahan/"
-          />
-          <TeamMemberCard
-            image="/images/Rameen.png"
-            name="Rameen"
-            role="Partnerships Officer"
-            link="" /* Waiting for LinkedIn link */
-          />
-        </div>
-        {/* Skeleton for Meet the Team Page */}
       </div>
-    </div>
+      </div>
   );
 }
